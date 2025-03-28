b97c5e165d8b9b6d020ac676fd2bcf1cc941bb1b
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-03-28 13:29:24 +0100

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all single digit
+ * numbers of base 10 from 0 followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 9; number++)
+	{
+		printf("%d", number);
+	}
+
+	printf("\n");
+
+	return (0);
+}
