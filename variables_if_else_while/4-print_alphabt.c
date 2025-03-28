a7741fe72f95c3e23a20758260ee2ea0448f4033
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-03-28 11:56:44 +0100

    nesting

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all letters of the alphabet in lowercase
+ *			excepting q and e followed by a new line.
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' & letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
