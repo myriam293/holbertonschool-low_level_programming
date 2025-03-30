b82397afd6c060ea02d86e87a7db47cdf0a36446
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-03-30 16:00:16 +0100

    6-print_numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * This programme prints all single digit numbers of base 10
+ * starting from 0, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+}
