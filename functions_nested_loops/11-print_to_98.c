d43c3993f1e5c901012ffae34ca8898d710a565c
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-01 15:42:58 +0100

    update to 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,28 +9,28 @@
 
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        for (; n <= 98; n++)
-        {
-            printf("%d", n);
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
 
-            if (n == 98)
-                continue;
-            printf(", ");
-        }
-        printf("\n");
-    }
-    else
-    {
-        for (; n >= 98; n--)
-        {
-            printf("%d", n);
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
 
-            if (n == 98)
-                continue;
-            printf(", ");
-        }
-        printf("\n");
-    }
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
 }
