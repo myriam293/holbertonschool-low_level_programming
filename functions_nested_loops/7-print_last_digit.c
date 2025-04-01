50b0bc70531737c39b6d26235570a7a5927d1a56
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-01 15:09:49 +0100

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,13 @@
  */
 int print_last_digit(int a)
 {
-    int n;
+	int n;
 
-    if (a < 0)
-        n = -1 * (a % 10);
-    else
-        n = a % 10;
+	if (a < 0)
+		n = -1 * (a % 10);
+	else
+		n = a % 10;
 
-    _putchar((n % 10) + '0');
-    return (n % 10);
+	_putchar((n % 10) + '0');
+	return (n % 10);
 }
