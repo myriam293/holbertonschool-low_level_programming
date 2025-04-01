fbe4171367de9e59d2dc6f685eef6951796738d9
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-01 14:49:17 +0100

    absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - This programme computes the absolute value of an integer
+ * @r: An integre input
+ *
+ * Return: Absolut value of number n.
+ */
+int _abs(int n)
+{
+    if (n >= 0)
+        return (n);
+    else
+        return (n * -1);
+}
