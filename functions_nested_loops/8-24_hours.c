482384047dff77a13deb032c7849e12ca7b7e7bf
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-01 15:19:49 +0100

    8-24_hours.c change

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,18 +8,18 @@
 
 void jack_bauer(void)
 {
-    int H, M;
+	int H, M;
 
-    for (H = 0; H < 24; H++)
-    {
-        for (M = 0; M < 60; M++)
-        {
-            _putchar((H / 10) + '0');
-            _putchar((H % 10) + '0');
-            _putchar(':');
-            _putchar((M / 10) + '0');
-            _putchar((M % 10) + '0');
-            _putchar('\n');
-        }
-    }
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
