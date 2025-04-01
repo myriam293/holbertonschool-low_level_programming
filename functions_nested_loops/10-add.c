2f7c4b2c3d7872b24341b298f62ebf90b779f267
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-01 15:34:26 +0100

    sum

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - This programme adds two integers and returns the result
+ * @a: An integer a
+ * @b: An integer b
+ *
+ * Return: The sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
