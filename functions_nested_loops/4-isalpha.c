aeb77556156333a29e32a3ecfa5de67758085d4a
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-01 14:31:03 +0100

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isalpha - This programme checks if a character is an alphabetic letter
+ * @c: An input character
+ *
+ * Return: 1 if c is a letter (uppercase or lowercase), 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+    char lower, upper;
+    int isletter = 0;
+
+    for (lower = 'a'; lower <= 'z'; lower++)
+    {
+        for (upper = 'A'; upper <= 'Z'; upper++)
+        {
+            if (c == lower || c == upper)
+                isletter = 1;
+        }
+    }
+    return (isletter);
+}
