69afe9b359bca82bb734f12c92eb714266c087ea
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-01 14:32:59 +0100

    updated

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -8,16 +8,16 @@
 
 int _isalpha(int c)
 {
-    char lower, upper;
-    int isletter = 0;
+	char lower, upper;
+	int isletter = 0;
 
-    for (lower = 'a'; lower <= 'z'; lower++)
-    {
-        for (upper = 'A'; upper <= 'Z'; upper++)
-        {
-            if (c == lower || c == upper)
-                isletter = 1;
-        }
-    }
-    return (isletter);
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == lower || c == upper)
+				isletter = 1;
+		}
+	}
+	return (isletter);
 }
