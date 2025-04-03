1b905119ebac13805d4457022df6b56f9ea65955
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-03 14:29:06 +0100

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - function copies the string pointed.
+ *
+ * @dest: pointer to copy the string to.
+ * @src: the string to copy to the destination.
+ *
+ * Return: a copy of the src.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (*src != '\0')
+	{
+		dest[index] = *src;
+		index++;
+		src++;
+	}
+	return (dest);
+}
