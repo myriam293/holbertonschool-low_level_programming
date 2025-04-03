f356c90c96128a85f123757040cf301f04887bc7
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-03 10:53:48 +0100

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+/**
+ * puts2 - a function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: An input string
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+	int length = 0, a = 0;
+
+	while (str[length] != '\0')
+		length++;
+
+	length -= 1;
+
+	for (; a <= length; a += 2)
+		_putchar(str[a]);
+
+	_putchar('\n');
+}
