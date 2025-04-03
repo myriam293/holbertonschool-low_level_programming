f8fffe28cdd01ec6c5a24ec30cd43e23c7bf6b1e
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-03 10:57:28 +0100

    ammends 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
-/**
+#include <stdio.h>
+#include "main.h" /**
  * puts2 - a function that prints every other character of a string,
  * starting with the first character, followed by a new line.
  * @str: An input string
