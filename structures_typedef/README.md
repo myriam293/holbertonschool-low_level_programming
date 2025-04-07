653e7d6bbacfa54d228361a45bfcd48c07999ba5
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-07 16:12:57 +0100

    define header file

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+Reaaaad meeeeee
---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef INV_TREE_H
+#define INV_TREE_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - defines dog's name, age and owner.
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: This programme defines dog's name, age and owner.
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
