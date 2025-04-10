7258113bece4fba5946bc29d9a9d1c2cf19ae83a
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-10 14:43:32 +0100

    task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_dlistint - A function that frees a dlistint_t list.
+ *
+ * @head: The double pointer to the head
+ *
+ * Return: Nothing.
+ */
+
+void free_dlistint(dlistint_t *head);
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
