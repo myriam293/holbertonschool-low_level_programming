e0a104dcdd0f2860799d83237dbcccdd355507c7
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-11 15:24:24 +0100

    task 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - A function that returns
+ * the length of a string.
+ *
+ * @s: An input string to printing
+ *
+ * Return: The length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
