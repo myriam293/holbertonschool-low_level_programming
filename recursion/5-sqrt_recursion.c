419d4bd66c799c727aa18bcfea3de22f9c51c223
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-13 16:00:52 +0100

    task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - It returns the value of square root of n.
+ *
+ * @n: an input integer
+ *
+ * Return: The square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (_sqrt(1, n));
+}
